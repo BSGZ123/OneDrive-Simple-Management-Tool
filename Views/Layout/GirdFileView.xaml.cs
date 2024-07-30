9ef80d427d3b24e91fc8a3ebe9f9b6452c846918
BsKPLu <bianshuiguangzhong@gmail.com>
2024-07-31 00:48:39 +0800

    feat: 网盘文件页面-为文件及目录条目设定上下文右键菜单

---
+++ Views/Layout/GirdFileView.xaml.cs
@@ -0,0 +1,53 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace OneDrive_Simple_Management_Tool.Views.Layout
+{
+    public sealed partial class GirdFileView : UserControl
+    {
+        public GirdFileView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ShowDeleteFileDialogAsync(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowConverFiletDialogAsync(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowShareFileDialogAsync(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowRenameFileDialogAsync(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowPropertyDialogAsync(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
