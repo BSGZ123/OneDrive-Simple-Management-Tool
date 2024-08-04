6c9ac47101df8475ab814703ef0d50cc903ead18
BsKPLu <bianshuiguangzhong@gmail.com>
2024-08-04 12:45:31 +0800

    feat: 云盘文件页面-云盘行页面组件与文件行组件组合构建云盘文件列表。

---
+++ Views/Layout/ImageCloudView.xaml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using OneDrive_Simple_Management_Tool.ViewModels;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace OneDrive_Simple_Management_Tool.Views.Layout
+{
+    public sealed partial class ImageCloudView : UserControl
+    {
+        public ImageCloudView()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void LoadIamgeAsync(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            if (!args.InRecycleQueue)
+            {
+                FileViewModel file = (FileViewModel)args.Item;
+                await file.LoadImage();
+            }
+        }
+
+        private async void LoadAllImages(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            foreach (FileViewModel image in (DataContext as DriveViewModel).Images.ToList())
+            {
+                await image.LoadImage();
+            }
+        }
+    }
+}
