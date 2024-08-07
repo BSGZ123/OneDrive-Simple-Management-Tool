10c1ab8a8297b56073c4417b4e99679368f1cdbd
BsKPLu <bianshuiguangzhong@gmail.com>
2024-08-07 22:53:18 +0800

    feat: 云盘传输功能-传输任务页面基础设计

--- ViewModels/TaskManagerViewModel.cs
+++ ViewModels/TaskManagerViewModel.cs
@@ -1,8 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace OneDrive_Simple_Management_Tool.ViewModels
 {
     public class TaskManagerViewModel : ObservableObject
     {
+        public ObservableCollection<DownloadTaskViewModel> DownloadTasks { get; } = new();
+        public ObservableCollection<UploadTaskViewModel> UploadTasks { get; } = new();
+
+
     }
 }
---
+++ ViewModels/UploadTaskViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneDrive_Simple_Management_Tool.ViewModels
+{
+    public class UploadTaskViewModel : ObservableObject
+    {
+
+    }
+}
