29b2c59aca9ff09ac85041c1c4bfc94f78833186
BsKPLu <bianshuiguangzhong@gmail.com>
2024-08-08 21:15:37 +0800

    feat: 云盘传输功能-下载参数设定及逻辑1

--- ViewModels/DownloadTaskViewModel.cs
+++ ViewModels/DownloadTaskViewModel.cs
@@ -1,14 +1,55 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using Downloader;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Microsoft.UI.Dispatching;
 using System.Threading.Tasks;
+using Windows.Storage;
+using Microsoft.Graph.Models;
 
 namespace OneDrive_Simple_Management_Tool.ViewModels
 {
-   public class DownloadTaskViewModel: ObservableObject
+   public partial class DownloadTaskViewModel: ObservableObject
     {
+        public DownloadTaskViewModel(DriveViewModel drive,  string itemId , StorageFile file)
+        {
+            _itemId = itemId;
+            _file = file;
+            Drive = drive;
+        }
 
+        public async Task StartDownload()
+        {
+            DriveItem item = await Drive.Provider.GetItem(_itemId);
+        }
+
+
+        //分片大小为1MB
+        public static readonly int chunkSize = 1024 * 1024;
+        private DateTime _lastUpdate;
+        //每秒更新当前下载速度
+        private readonly TimeSpan _updateInterval = TimeSpan.FromMilliseconds(1000);
+        private readonly string _itemId;
+        private DriveViewModel Drive { get; }
+        private readonly StorageFile _file;
+        //确保整个程序只有一个taskmanager实例
+        private readonly TaskManagerViewModel _manager = Ioc.Default.GetService<TaskManagerViewModel>();
+        private DownloadService _downloader;
+        private DownloadPackage _pack;
+        //管理 UI 线程上的操作,保证UI线程持续保持响应
+        private readonly DispatcherQueue _dispatcher = DispatcherQueue.GetForCurrentThread();
+
+
+        [ObservableProperty] private int _progress;
+        [ObservableProperty] private bool _completed = false;
+        [ObservableProperty] private bool _isDownloading = true;
+        [ObservableProperty] private bool _isPaused = false;
+        [ObservableProperty] private long _downloadedBytes = 0;
+        [ObservableProperty] private long _totalBytes = 0;
+        [ObservableProperty] private long _downloadSpeed = 0;
+
+        public DateTime StartTime { get; private set; }
+        public DateTime FinishTime { get; private set; }
+        public string Name { get => _file.Name; }
     }
 }
--- ViewModels/TaskManagerViewModel.cs
+++ ViewModels/TaskManagerViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.ObjectModel;
+using Windows.Storage;
+using System.Threading.Tasks;
 
 namespace OneDrive_Simple_Management_Tool.ViewModels
 {
@@ -8,6 +10,11 @@ namespace OneDrive_Simple_Management_Tool.ViewModels
         public ObservableCollection<DownloadTaskViewModel> DownloadTasks { get; } = new();
         public ObservableCollection<UploadTaskViewModel> UploadTasks { get; } = new();
 
-
+        public async Task AddDownloadTask(DriveViewModel drive,string itemId,StorageFile file)
+        {
+            DownloadTaskViewModel downloadTask = new(drive, itemId, file);
+            DownloadTasks.Add(downloadTask);
+            await downloadTask.StartDownload();
+        }
     }
 }
