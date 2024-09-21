5531a0f1be6dd52dbc1ef2f5955ec79c20fc1473
BsKPLu <bianshuiguangzhong@gmail.com>
2024-09-21 17:12:54 +0800

    feat: 云盘工具页-工具卡片数据模型设计及页面准备

---
+++ Models/ToolItem.cs
@@ -0,0 +1,11 @@
+﻿
+namespace OneDrive_Simple_Management_Tool.Models
+{
+    public class ToolItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public string FileName { get; set; }
+    }
+}
