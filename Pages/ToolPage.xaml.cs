ec1f61a475c49cdeaf5293653571ec75a05ff0ac
BsKPLu <bianshuiguangzhong@gmail.com>
2024-09-29 00:52:54 +0800

    feat: 云盘工具页-工具卡片组模板页面完成

--- Pages/ToolPage.xaml.cs
+++ Pages/ToolPage.xaml.cs
@@ -12,6 +12,10 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System.Windows;
+using OneDrive_Simple_Management_Tool.Models;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -21,11 +25,49 @@ namespace OneDrive_Simple_Management_Tool.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ToolPage : Page
+    public  partial class ToolPage : Page,INotifyPropertyChanged
     {
         public ToolPage()
         {
             this.InitializeComponent();
+        }
+
+        //用于设置属性并通知 UI 更改
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)//此属性自动获取调用属性的名称
+        {
+            if (Equals(storage, value)) return false;
+
+            storage = value;
+            NotifyPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        //这个后续添加只能手动了
+        private IEnumerable<ToolItem> _items =new List<ToolItem> {
+        
+            new() {
+                Name = "Share Community",
+                Description="Share and browse OneDrive files",
+                ImagePath="/Assets/link-share.png",
+                FileName="ShareCommunity"
+            },
+        };
+
+        public IEnumerable<ToolItem> Items
+        {
+            get => _items;
+            set => SetProperty(ref _items, value);
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //点击工具卡片后导航至目标页面
+        private void GridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            MessageBox.Show("666666666");
         }
     }
 }
