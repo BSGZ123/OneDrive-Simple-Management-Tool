270acf521321650588f35ec8570855d93a42c1d1
BsKPLu <bianshuiguangzhong@gmail.com>
2024-12-22 22:59:33 +0800

    feat: 行文件页面预览处理及markdown文件预览测试

---
+++ ViewModels/PreviewViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace OneDrive_Simple_Management_Tool.ViewModels.Tools
+{
+    public class PreviewViewModel : ObservableObject
+    {
+        public PreviewViewModel(FileViewModel viewModel) { _file = viewModel; }
+
+
+        private readonly FileViewModel _file;
+    }
+}
--- Views/Layout/ColumnFileView.xaml.cs
+++ Views/Layout/ColumnFileView.xaml.cs
@@ -1,8 +1,14 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
+using OneDrive_Simple_Management_Tool.Models;
+using OneDrive_Simple_Management_Tool.Services;
 using OneDrive_Simple_Management_Tool.ViewModels;
+using OneDrive_Simple_Management_Tool.ViewModels.Tools;
+using OneDrive_Simple_Management_Tool.Views.Preview;
 using System;
+using System.IO;
+using System.Threading.Tasks;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -61,9 +67,29 @@ namespace OneDrive_Simple_Management_Tool.Views.Layout
 
         }
 
-        private void ShowPreviewDialogAsync(object sender, RoutedEventArgs e)
+        //弹出预览页面(处理选中文件viewmodel)
+        private async void ShowPreviewDialogAsync(object sender, RoutedEventArgs e)
         {
+            FileViewModel viewModel = DataContext as FileViewModel;
+            await ShowPreviewDialogFromViewModel(viewModel);
+        }
 
+        private async Task ShowPreviewDialogFromViewModel(FileViewModel viewModel)
+        {
+            //获得选中文件路径字符串的扩展名(转为小写)并判断文件类型 合适的进行预览
+            switch (Utils.GetFileType(Path.GetExtension(viewModel.Name).ToLower()))
+            {
+                case FileType.Markdown:
+                    {
+                        MarkdownPreviewView dialog = new()
+                        {
+                            XamlRoot = XamlRoot,
+                            DataContext = new PreviewViewModel(viewModel)
+                        };
+                        await dialog.ShowAsync();
+                        break;
+                    }
+            }
         }
 
         private async void OpenFile(object sender, DoubleTappedRoutedEventArgs e)
@@ -75,7 +101,7 @@ namespace OneDrive_Simple_Management_Tool.Views.Layout
             }
             else 
             {
-                //这里后续是打开文件，常见格式文件预览
+                //这里后续是打开文件后常见格式文件直接预览
                 
             }
         }
